0888e2d6dd3375c7f14fbf661f9bbf0a8bf3f381
ymkashif <74010209+ymkashif@users.noreply.github.com>
2020-11-27 21:05:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+
+function setup(){
+  var canvas=createCanvas(400,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(0);
+  rectMode(CENTER)  
+  rect(200,200,50,50);
+}
